104df928d576b02f0b5ce742eb8fb08329b823d3
FRANK\User <wishes10707@gmail.com>
2023-10-09 11:30:03 +0800

    將 error log 與一般 log 分離

--- src/main/java/com/hung/controller/GlobalExceptionHandler.java
+++ src/main/java/com/hung/controller/GlobalExceptionHandler.java
@@ -1,19 +1,24 @@
 package com.hung.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.servlet.ModelAndView;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
+	
+	private static final Logger log = LoggerFactory.getLogger(GlobalExceptionHandler.class);
 
     @ExceptionHandler(Exception.class)
     public ModelAndView handleAllExceptions(Exception e){
         ModelAndView modelAndView = new ModelAndView();
 
         modelAndView.addObject("msg", e.getMessage());
-        modelAndView.setViewName("excep");
+        modelAndView.setViewName("exception");
 
+        log.error("[錯誤產生] - " + e.getMessage());
         return modelAndView;
     }
 
